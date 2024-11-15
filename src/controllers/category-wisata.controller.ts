c1e0d21cd10fa9946a1b1595553bd73dae134667
ekrtna <ekaratna414@gmail.com>
2024-11-15 20:13:10 +0700

    Added new controller

--- src/controllers/category-wisata.controller.ts
+++ src/controllers/category-wisata.controller.ts
@@ -5,25 +5,45 @@ import { formatValidationErrors } from '../utility/validation.utility';
 import { myPrisma } from '../config/db.config';
 import { asyncError } from '../middleware/global-error.middleware';
 
-export const CreateCategoryWisata: any = asyncError(async (req: Request, res: Response) => {
-    const category_wisata = await myPrisma.categoryWisata.create({
+export const CreateProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const provinsi = await myPrisma.provinsi.create({
         data: {
             nama: req.body.nama,
             thumbnail: req.body.thumbnail
         }
     });
 
-    res.send(category_wisata)
+    res.send(provinsi);
 });
 
-export const GetCategoryWisata: any = asyncError(async (req: Request, res: Response) => {
-
+export const GetProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const category = await myPrisma.categoryWisata.findUnique({
+        where: { id: Number(req.params.id) }
+    });
+    res.status(200).send(category);
 });
 
-export const UpdateCategoryWisata: any = asyncError(async (req: Request, res: Response) => {
+export const UpdateProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { nama, thumbnail } = req.body;
 
+    const updatedCategory = await myPrisma.categoryWisata.update({
+        where: { id: Number(id) },
+        data: {
+            nama,
+            thumbnail
+        }
+    });
+
+    res.send({ pesan: "Berhasil diupdate", data: updatedCategory });
 });
 
-export const DeleteCategoryWisata: any = asyncError(async (req: Request, res: Response) => {
+export const DeleteProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const { id } = req.params;
 
+    await myPrisma.categoryWisata.delete({
+        where: { id: Number(id) }
+    });
+
+    res.send({ pesan: "Berhasil dihapus" });
 });
--- src/controllers/provinsi.controller.ts
+++ src/controllers/provinsi.controller.ts
@@ -17,13 +17,33 @@ export const CreateProvinsi: any = asyncError(async (req: Request, res: Response
 });
 
 export const GetProvinsi: any = asyncError(async (req: Request, res: Response) => {
-
+    const category = await myPrisma.categoryWisata.findUnique({
+        where: { id: Number(req.params.id) }
+    });
+    res.status(200).send(category);
 });
 
 export const UpdateProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { nama, thumbnail } = req.body;
 
+    const updatedCategory = await myPrisma.categoryWisata.update({
+        where: { id: Number(id) },
+        data: {
+            nama,
+            thumbnail
+        }
+    });
+
+    res.send({ pesan: "Berhasil diupdate", data: updatedCategory });
 });
 
 export const DeleteProvinsi: any = asyncError(async (req: Request, res: Response) => {
+    const { id } = req.params;
 
+    await myPrisma.categoryWisata.delete({
+        where: { id: Number(id) }
+    });
+
+    res.send({ pesan: "Berhasil dihapus" });
 });
